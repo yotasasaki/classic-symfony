80d7616e43fb6698d1447511fb341db4078aeb07
Yota Sasaki <yotsak83@gmail.com>
2016-01-27 12:22:04 +0000

    Create inquiry form. Set intercept redirects
    P.100-103

---
+++ src/AppBundle/Controller/InquiryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/inquiry")
+ */
+class InquiryController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Method("get")
+     */
+    public function indexAction()
+    {
+        return $this->render('Inquiry/index.html.twig',
+            ['form' => $this->createInquiryForm()->createView()]
+        );
+    }
+
+    /**
+     * @Route("/")
+     * @Method("post")
+     */
+    public function indexPostAction(Request $request)
+    {
+        $form = $this->createInquiryForm();
+        $form->handleRequest($request); //送信された情報をフォームオブジェクトに取り込む
+        if ($form->isValid())
+        {
+            return $this->redirect(
+                $this->generateUrl('app_inquiry_complete')
+            );
+        }
+
+        return $this->render('Inquiry/index.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+    /**
+     * @Route("/complete")
+     */
+    public function completeAction()
+    {
+        return $this->render('Inquiry/complete.html.twig');
+    }
+
+    private function createInquiryForm()
+    {
+        return $this->createFormBuilder()
+            ->add('name', 'text')
+            ->add('email', 'text')
+            ->add('tel', 'text', [
+                'required' => false
+            ])
+            ->add('type', 'choice', [
+                'choices' => [
+                    '公演について',
+                    'その他',
+                ],
+                'expanded' => true,
+            ])
+            ->add('content', 'textarea')
+            ->add('submit', 'submit', [
+                'label' => '送信',
+            ])
+            ->getForm();
+    }
+}
